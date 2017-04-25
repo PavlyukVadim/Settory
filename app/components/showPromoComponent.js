f5bf674d899ad27d74bebc1f4d85aaa270a9dfea
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-04-25 20:07:30 +0300

    showPromoComponent, addPromoCode, deletePromoCode

--- app/components/showPromoComponent.js
+++ app/components/showPromoComponent.js
@@ -5,22 +5,50 @@ function PromoCode(discount, dTime, code) {
 }
 
 var ShowPromo = React.createClass({
+  getInitialState: function() {
+    return {
+      arrayOfPromoCodes: this.props.items
+    }
+  },
+
   componentDidMount: function() {
     this.timepick();
   },
 
   timepick: function() {
-    $("#datapicker2").datepicker({
+    $('#datapicker2').datepicker({
       dateFormat: 'dd/mm/yy'
     });
   },
 
+  deletePromoCode: function(i) {
+    this.setState((prevState) => {
+      prevState.arrayOfPromoCodes.splice(i, 1);
+      return {
+        arrayOfPromoCodes: prevState.arrayOfPromoCodes
+      };
+    })
+  },
+
+  addPromoCode: function(e) {
+    e.preventDefault();
+    this.setState((prevState) => {
+      var newPromoCode = new PromoCode(this.discountInput.value,
+                                       this.dTimeInput.value,
+                                       this.codeInput.value);
+      prevState.arrayOfPromoCodes.push(newPromoCode);
+      return {
+        arrayOfPromoCodes: prevState.arrayOfPromoCodes
+      };
+    })
+  },
+
   getPromoCodes: function() {
-    var promoCodes = this.props.items.map(function(item, i) {
+    var promoCodes = this.state.arrayOfPromoCodes.map((item, i) => {
       return (
         <div className="promoCodShow" key={i}>
           <p>Промо на {item.discount}% дійсне до {item.dTime}, код:{item.code}</p>
-          <a className="deletepromo">Видалити</a>
+          <a className="deletepromo" onClick={() => this.deletePromoCode(i)}>Видалити</a>
         </div>
       );
     });
@@ -32,21 +60,31 @@ var ShowPromo = React.createClass({
       <div>
         <div className="wrapperPromoCreate">
           <h2>Промокоди</h2>
-          <form id="promoCreate">
+          <form id="promoCreate" onSubmit={this.addPromoCode}>
             <p className="control has-addons">
-              <input className="input" type="text" placeholder="процент" required />
+              <input className="input" type="text" 
+                     ref={(input) => { this.discountInput = input; }}
+                     placeholder="процент" 
+                     required />
               <a className="button">%</a>
             </p>
             <p className="control has-addons">
-              <input type="text" className="input" id="datapicker2" onClick={this.timepick} placeholder="день/місяць/рік" required />
+              <input type="text" className="input"
+                     id="datapicker2"
+                     onClick={this.timepick} 
+                     ref={(input) => { this.dTimeInput = input; }}
+                     placeholder="день/місяць/рік" 
+                     required />
               <a className="button">ДАТА</a>
             </p>
             <p className="control has-addons">
-              <input className="input" type="text" placeholder="код" />
+              <input className="input" type="text" 
+                     ref={(input) => { this.codeInput = input; }}
+                     placeholder="код" />
               <a className="button">КОД</a>
             </p>
             <p className="control">
-              <button className="button is-success" type="submit">Додати промо</button>
+              <button className="button is-success" type="submit" >Додати промо</button>
             </p>
           </form>
         </div>
