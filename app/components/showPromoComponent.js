0e3459b60e0018cff983daa5cb87a666b30ba7a1
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-04-25 17:00:18 +0300

    showPromoComponent

--- app/components/showPromoComponent.js
+++ app/components/showPromoComponent.js
@@ -1,49 +1,57 @@
-var ShowPromo = React.createClass({
-    dataPic: function(){
-        $("#datapicker2").datepicker({
-            dateFormat: 'dd.mm.yy'
-        });
-    },
+function PromoCode(discount, dTime, code) {
+  this.discount = discount;
+  this.dTime = dTime;
+  this.code = code;
+}
 
-    render: function(){
-         var promoArrShow =  this.props.items.map(function(item, i){
-             return (
-                <div className="promoCodShow" key={i}>
-                <p>Промо на {item.cof}% дійсне до {item.dTime} код:{item.mesg}</p>
-                <a className="deletepromo">Видалити</a>
-                </div>
-             )
-         });
-         return (
-            <div>
-                <div className="wrapperPromoCreate">
-                  <h2>Промокоди</h2>
-                  <form id="promoCreate">
-                    <p className="control has-addons">
-                      <input className="input" type="text" placeholder="процент" required />
-                      <a className="button">
-                        %
-                      </a>
-                    </p>
-                    <p className="control has-addons">
-                      <input type="text" className="input" required id="datapicker2" onClick={this.dataPic} placeholder="день/місяць/рік" />
-                      <a className="button">
-                        ДАТА
-                      </a>
-                    </p>
-                    <p className="control has-addons">
-                      <input className="input" type="text" placeholder="код" />
-                      <a className="button">
-                        КОД
-                      </a>
-                    </p>
-                    <p className="control">
-                      <button className="button is-success" type="submit">Додати промо</button>
-                    </p>
-                  </form>
-                </div>
-                <div className="promoShowContent">{ promoArrShow }</div>
-            </div>
-        )
-    },
+var ShowPromo = React.createClass({
+  componentDidMount: function() {
+    this.timepick();
+  },
+
+  timepick: function() {
+    $("#datapicker2").datepicker({
+      dateFormat: 'dd/mm/yy'
+    });
+  },
+
+  getPromoCodes: function() {
+    var promoCodes = this.props.items.map(function(item, i) {
+      return (
+        <div className="promoCodShow" key={i}>
+          <p>Промо на {item.discount}% дійсне до {item.dTime}, код:{item.code}</p>
+          <a className="deletepromo">Видалити</a>
+        </div>
+      );
+    });
+    return promoCodes;
+  },
+
+  render: function() {
+    return (
+      <div>
+        <div className="wrapperPromoCreate">
+          <h2>Промокоди</h2>
+          <form id="promoCreate">
+            <p className="control has-addons">
+              <input className="input" type="text" placeholder="процент" required />
+              <a className="button">%</a>
+            </p>
+            <p className="control has-addons">
+              <input type="text" className="input" id="datapicker2" onClick={this.timepick} placeholder="день/місяць/рік" required />
+              <a className="button">ДАТА</a>
+            </p>
+            <p className="control has-addons">
+              <input className="input" type="text" placeholder="код" />
+              <a className="button">КОД</a>
+            </p>
+            <p className="control">
+              <button className="button is-success" type="submit">Додати промо</button>
+            </p>
+          </form>
+        </div>
+        <div className="promoShowContent">{this.getPromoCodes()}</div>
+      </div>
+    )
+  }
 });
