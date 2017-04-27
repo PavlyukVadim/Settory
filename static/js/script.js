73ce04465b4eda2391afaad8723632d8c203294a
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-04-27 23:38:08 +0300

    fix first screen

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     autoControls: true,
     touchEnabled: false,
     controls: false,
-    pause: 5000
+    pause: 3000
   });
 });
 
@@ -33,8 +33,38 @@ function fadeOutnojquery(el) {
   },16);
 }
 
-window.onload = function() {
-  setTimeout(function() {
-    fadeOutnojquery(hellopreloader);
-  }, 100);
+var slider = {
+  slides: ['static/img/example_img_1.jpg', 'static/img/example_img_5.jpg', 'static/img/example_img_6.jpg'],
+  titleArr: ['для житлових кімнат','для кухні', 'для ванної та туалету', 'для коридору'],
+  frame: 0,
+  set: function (image, title) { // установка нужного фона слайдеру
+    setTimeout(function () {
+    document.getElementById('sliderTitle').style.opacity = "1";
+    document.getElementById('sliderTitle').innerHTML="" + title + "";
+    document.getElementById('sliderTitle').style.transition = "opacity 0.5s linear"}, 500)
+  },
+  init: function () {
+    this.set(this.slides[this.frame], this.titleArr[this.frame]);
+  },
+  left: function () {
+    this.frame--;
+    if (this.frame < 0) this.frame = this.slides.length - 1;
+    this.set(this.slides[this.frame], this.titleArr[this.frame]);
+  },
+  right: function () {
+    this.frame++;
+    if (this.frame == this.slides.length) this.frame = 0;
+    document.getElementById('sliderTitle').style.opacity = "0";
+    document.getElementById('sliderTitle').style.transition = "opacity 0.5s linear";
+    this.set(this.slides[this.frame], this.titleArr[this.frame]); 
+  }
 };
+
+slider.init();
+setInterval(function () {
+  slider.right();
+}, 3000);
+
+setTimeout(function() {
+  fadeOutnojquery(hellopreloader);
+}, 100);
