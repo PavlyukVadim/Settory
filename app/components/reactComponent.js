7a39378b031f2981fae94cf3f7344bef9f32d6cd
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-02 15:43:22 +0300

    code style

--- app/components/reactComponent.js
+++ app/components/reactComponent.js
@@ -20,47 +20,81 @@ var Example = React.createClass({
   }
 });
 
-
 var userBox = 1;
 ReactDOM.render(<MyHeader user={userBox}/>, $('header')[0]);
 ReactDOM.render(<OrderBox/>, document.getElementById('reactBox'));
 ReactDOM.render(<MyFooter/>, $('footer')[0]);
 
 var i = 0;
-var j =0;
+var j = 0;
 
-function dataCod(mail,dTime,phone) {
+function userData(mail, dTime, phone) {
   this.mail = mail;
   this.dTime = dTime;
   this.phone = phone;
 }
 
-function orderCod(numberValue, mail, phone, adress, room, dTimeH, dTimeD, option, pay, status) {
-  this.numberValue = numberValue;
-  this.mail = mail;
-  this.phone = phone;
-  this.adress = adress;
-  this.room = room;
-  this.dTimeH = dTimeH;
-  this.dTimeD = dTimeD;
-  this.option = option;
-  this.pay = pay;
-  this.status = status;
+function orderData(orderObj) {
+  this.orderNumber = orderObj.orderNumber;
+  this.mail = orderObj.mail;
+  this.phone = orderObj.phone;
+  this.address = orderObj.address;
+  this.numberOfRooms = orderObj.numberOfRooms;
+  this.time = orderObj.time;
+  this.date = orderObj.date;
+  this.options = orderObj.options;
+  this.price = orderObj.price;
+  this.status = orderObj.status;
 }
 
-var order1 = new orderCod(1,'blabla@mail.com','0935556644','ShitIt st',2,'20:16','2k16',[1,2],500,false);
-var order2 = new orderCod(2,'albalb@mail.com','0932223311','ShitHi st',1,'20:15','2k14',[1,4],600,false);
-var order3 = new orderCod(3,'lablab@mail.com','0507778899','ShitHer st',3,'20:20','2k15',[1,3],700,true);
+var order1 = new orderData({
+  orderNumber: 1,
+  mail: 'sample.mail1@mail.com',
+  phone: '0935556644',
+  address: 'Atlantic City, NJ 08401',
+  numberOfRooms: 2,
+  time: '20:16',
+  date: '21/12/2016',
+  options: [1, 2],
+  price: 500,
+  status: 'Aктивно'
+});
 
-var dataArr = [new dataCod('1asdasf@mail','2012-10-10','000665'),
-               new dataCod('2f@mail','2012-11-11','000665555'),
-               new dataCod('3f@mail','2012-12-12','000')];
+var order2 = new orderData({
+  orderNumber: 2,
+  mail: 'sample.mail1@mail.com',
+  phone: '093556544',
+  address: 'Atlantic City, NJ 08546',
+  numberOfRooms: 1,
+  time: '20:15',
+  date: '22/02/2009',
+  options: [1, 4],
+  price: 600,
+  status: 'Завершено'
+});
 
-var promoArr = [new PromoCode('10', '2012-10-10', 'Blablabla1'),
-                new PromoCode('20', '2012-11-11', 'Blablabla2'),
-                new PromoCode('30', '2012-12-12', 'Blablabla3')];
+var order3 = new orderData({
+  orderNumber: 3,
+  mail: 'sample.mail1@mail.com',
+  phone: '093556654',
+  address: 'Atlantic City, NJ 65335',
+  numberOfRooms: 3,
+  time: '12:56',
+  date: '22/02/2019',
+  options: [1, 3],
+  price: 700,
+  status: 'Очікується'
+});
 
-var orderArr = [order1,order2,order3,
+var dataArr = [new userData('sample.mail1@mail.com','2012-10-10','0935556644'),
+               new userData('sample.mail2@mail.com','2012-11-11','0932223311'),
+               new userData('sample.mail3@mail.com','2012-12-12','0507778899')];
+
+var promoArr = [new PromoCode('10', '2012-10-10', 'PromoCode1'),
+                new PromoCode('20', '2012-11-11', 'PromoCode2'),
+                new PromoCode('30', '2012-12-12', 'PromoCode3')];
+
+var ordersArr = [order1,order2,order3,
                 order1,order2,order3,
                 order1,order2,order3,
                 order1,order2,order3,
@@ -69,15 +103,15 @@ var orderArr = [order1,order2,order3,
                 order1,order2,order3,
                 order1,order2,order3];
 
-var spliceArr = [];
+/*var spliceArr = [];
 var copyArr = orderArr.slice();
 
 do {
   spliceArr.push(copyArr.splice(0,20))
 } while(copyArr[0] !== undefined);
+*/
 
-
-var ShowOrderWrap = React.createClass({
+var AllOrdersWrap = React.createClass({
   render: function(){
     return (
         <div>
@@ -86,7 +120,7 @@ var ShowOrderWrap = React.createClass({
               <p><strong>Замовлення</strong> Опрацювати кожне необхідно протягом 5 хвилин</p>
             </div>
           </div>
-            <ShowOrder itemsIn={spliceArr}  />
+            <AllOrders ordersArr={ordersArr}  />
         </div>
       )
     }
@@ -102,14 +136,14 @@ $(".orders-link").click(function() {
   $('#reactBox').empty();
   $('.nav-item').removeClass("is-active");
   $('.orders-link').addClass("is-active");
-  ReactDOM.render(<ShowOrderWrap/>, document.getElementById('reactBox'));
+  ReactDOM.render(<AllOrdersWrap/>, document.getElementById('reactBox'));
 });
 
 var pure = function() {
   $('#reactBox').empty();
   $('.nav-item').removeClass("is-active");
   $('.orders-link').addClass("is-active");
-  ReactDOM.render(<ShowOrderWrap />, document.getElementById('reactBox'));
+  ReactDOM.render(<AllOrdersWrap />, document.getElementById('reactBox'));
 };
 
 var pureFilter = function(bool1) {
