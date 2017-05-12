1719b1f011445176070f97a38b70a5b8f8236fc0
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-12 18:56:54 +0300

    configured routes in navbar

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -6,46 +6,32 @@ import './Header.css';
 class Header extends Component {
 
   render() {
-    if (!~window.location.href.indexOf('#/user/')) {
-      return (
-        <header>
-          <div className="wrapperHeader ">
-            <div className="navbar">
-              <div className="navbarLogo">
-                <Link to="/admin/newOrder">
-                  <img className="logo" src="./static/img/logo.png"></img>
-                </Link>
-              </div>
-              <div className="mobile">
-                <NavLink to="/admin/promoCodes" activeClassName="selected" className="nav-item">Промокод</NavLink>
-                <NavLink to="/admin/allUsers" activeClassName="selected" className="nav-item">Користувачі</NavLink>
-                <NavLink to="/admin/allOrders" activeClassName="selected" className="nav-item">Замовлення</NavLink>
-                <NavLink to="/admin/yourOrders" activeClassName="selected" className="nav-item">Ваші прибирання</NavLink>
-                <a className="nav-item" href="../">Вийти</a>  
-              </div>
+    let isAdmin = !~window.location.href.indexOf('#/user/');
+    let mediateRoute = isAdmin ? '/admin' : '/user';
+    return (
+      <header>
+        <div className="wrapperHeader ">
+          <div className="navbar">
+            <div className="navbarLogo">
+              <Link to={`${mediateRoute}/newOrder`}>
+                <img className="logo" src="./static/img/logo.png"></img>
+              </Link>
+            </div>
+            <div className="mobile">
+              {isAdmin &&
+                <div className="admin-nav-links">
+                  <NavLink to={`${mediateRoute}/promoCodes`} activeClassName="selected" className="nav-item">Промокод</NavLink>
+                  <NavLink to={`${mediateRoute}/allUsers`} activeClassName="selected" className="nav-item">Користувачі</NavLink>
+                  <NavLink to={`${mediateRoute}/allOrders`} activeClassName="selected" className="nav-item">Замовлення</NavLink>
+                </div>
+              }
+              <NavLink to={`${mediateRoute}/yourOrders`} activeClassName="selected" className="nav-item">Ваші прибирання</NavLink>
+              <a className="nav-item" href="../">Вийти</a>  
             </div>
           </div>
-        </header>
-      )
-    } else {
-      return (
-        <header>
-          <div className="wrapperHeader ">
-            <div className="navbar">
-              <div className="navbarLogo">
-                <Link to="/user/newOrder">
-                  <img className="logo" src="./static/img/logo.png"></img>
-                </Link>
-              </div>
-              <div className="mobile">
-                <NavLink to="/user/yourOrders" activeClassName="selected" className="nav-item">Ваші прибирання</NavLink>
-                <a className="nav-item" href="../">Вийти</a>  
-              </div>
-            </div>
-          </div>
-        </header>
-      )
-    }
+        </div>
+      </header>
+    )
   }
 }
 
